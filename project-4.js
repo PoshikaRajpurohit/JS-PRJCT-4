2bb58cdd60ecc7704e5d15db879d5bf5ee84dcb1
PoshikaRajpurohit <159758700+PoshikaRajpurohit@users.noreply.github.com>
2024-12-25 11:28:19 +0530

    Add files via upload

---
+++ project-4.js
@@ -0,0 +1,148 @@
+// UDF PROJECT
+
+
+// #1 Greeting-Message
+
+    // function greet(message) {
+    //     console.log("GOOD MORNING");
+    // }
+    // greet();
+
+
+    // EXPECTED-OUTPUT
+    // "GOOD-MORNING"
+
+
+
+// #2 Area of Circle using return with argument.
+
+    // function circle(r){
+    //     let area=3.14*r*r;
+    //     return area;
+    // }
+    // console.log("Area of Circle=",(circle(5)));
+
+
+
+    // EXPECTED-OUTPUT
+    // Area of Circle=78.5
+
+
+
+// #3 Area of Triangle using return with argument.
+
+    // function triangle(l,h){
+    //     let area=l*h/2;
+    //     return area;
+    // }
+    // console.log("Area of Triangle=",(triangle(15,12)));
+
+
+    // EXPECTED-OUTPUT
+    // Area of Triangle=90
+
+
+// #4 Area of Rectangle using return with argument.
+
+    // function rectangle(l,h){
+    //         let area=l*h;
+    //     return area;
+    // }
+    // console.log("Area of Rectangle=",(rectangle(10,12)));
+
+
+
+    // EXPECTED-OUTPUT
+    // Area of Rectangle=120
+
+
+
+// #5 Ans of ((b*b)(4*a*c))/2*c)
+
+    // function equation(a,b,c){
+    //     let equation=(b*b*4*a*c)/(2*a);
+    //     return equation;
+    // }
+    // console.log("Answer of the equation=",(equation(15,20,8)));
+
+
+
+    // EXPECTED-OUTPUT
+    // Answer of equation=6400
+
+
+
+// #6 Ans of (a*a)+(2*a*b)+(b*b)
+
+    // function equation(a,b){
+    //     let equation=(a*a)+(2*a*b)+(b*b);
+    //     return equation;
+    // }
+    // console.log("Answer of the equation=",(equation(15,10)));
+
+
+    // EXPECTED-OUTPUT
+    // Answer of equation=625
+
+
+// #7 Farehnit to Celsius
+
+    // function fc(f){
+    //     let formula=(f-32/1.8);
+    //     return formula ; 
+    //  }
+    //      console.log("Conversion of Farehnit into Celsius =",(fc(40)));
+
+
+
+    // EXPECTED-OUTPUT
+    // Farehnit into Celsius=22.22222222222222
+
+
+// #8 Celsius to Farehnit
+
+    // function cf(c){
+    //         let formula=(c*1.8+32);
+    //         return formula ;
+    // }
+    // console.log("Conversion of Celsius into Farehnit =",(cf(20)));
+
+
+
+    // EXPECTED-OUTPUT
+    // Celsius to Farehnit=68
+
+
+
+// #9 Number is odd or even.
+
+    // function check(num){
+    //     if(num%2==0){
+    //         console.log("The given number is even number");
+    //     }else{
+    //         console.log("The given number is odd number");
+    //     }
+    // }
+    // check(2);
+    // check(3);
+
+    // EXPECTED-OUTPUT
+    // Number=2 output=The given number is even number
+    // Number=3 output=The given number is odd number
+
+
+
+// #10 Swap Variable without 3rd variable.
+    // function swap(a,b){
+    //    console.log("Value before swapping:a=",a,"b=",b);
+    //    a=a+b;
+    //    b=a-b;
+    //    a=a-b;
+    //    console.log("Value after swapping:a=",a,"b=",b);
+        
+    //}
+//     swap(10,5)
+
+    // EXPECTED-OUTPUT
+    // VALUE BEFORE SWAP=10,5
+    // VALUE AFTER SWAP=5,10
